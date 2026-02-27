072d69924f9437a8648e64bfde16620e3f92795b
Galeih <florian.sauvage.etudiant@gmail.com>
2026-02-27 11:13:09 +0100

    config github pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const repo = "REPO_NAME"; // <= remplace par le nom du repo GitHub
+const repo = "supa-blog";
 
 const nextConfig: NextConfig = {
-  output: "export", // static export :contentReference[oaicite:3]{index=3}
-  images: { unoptimized: true }, // l'optimisation d'images nécessite un serveur :contentReference[oaicite:4]{index=4}
-  trailingSlash: true, // pratique sur GH Pages :contentReference[oaicite:5]{index=5}
-  basePath: isProd ? `/${repo}` : "", // GH Pages sert sur /<repo> :contentReference[oaicite:6]{index=6}
+  output: "export", 
+  images: { unoptimized: true }, 
+  trailingSlash: true, 
+  basePath: isProd ? `/${repo}` : "", 
   assetPrefix: isProd ? `/${repo}/` : "",
 };
 
